ccf0f19d5e42c9bd7b1bde2631c7130de06528bf
SEANGLAY <devolutionary.wizard@gmail.com>
2024-10-21 13:16:58 +0700

    aba payway testing

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const crypto = require("node:crypto");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// constants
-
+// Constants
 const ABA_PAYWAY_API_URL = process.env.ABA_PAYWAY_API_URL;
 const ABA_PAYWAY_API_KEY = process.env.ABA_PAYWAY_API_KEY;
 const ABA_PAYWAY_MERCHANT_ID = process.env.ABA_PAYWAY_MERCHANT_ID;
 
-// middlewares
-app.use(bodyParser.urlencoded({ extended: true }));
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// generate HMAC hash
-
+// Generate HMAC hash
 function generateHash(data, publicKey) {
   const hmac = crypto.createHmac("sha512", publicKey);
   hmac.update(data);
-  const hash = hmac.digest("base64");
-  return hash;
+  return hmac.digest("base64");
 }
 
-// handle checkout form submission
+// Handle checkout form submission
 app.post("/checkout", (req, res) => {
   const items = Buffer.from(
     JSON.stringify([
@@ -36,7 +32,6 @@ app.post("/checkout", (req, res) => {
 
   const req_time = Math.floor(Date.now() / 1000);
   const tran_id = req_time;
-  const merchant_id = ABA_PAYWAY_MERCHANT_ID;
   const amount = "5.00";
   const firstName = "Makara";
   const lastName = "Prom";
@@ -45,38 +40,37 @@ app.post("/checkout", (req, res) => {
   const return_params = "Hello World!";
   const type = "purchase";
   const currency = "USD";
-  const payment_option = "abapay";
   const shipping = "1.50";
-  const cancel_url = "http://localhost:3000/cart/";
-  const continue_success_url = "http://localhost:3000/";
+  const cancel_url = `http://localhost:${PORT}/cart/`;
+  const continue_success_url = `http://localhost:${PORT}/`;
 
-  const dataToHash = `${req_time}${merchant_id}${tran_id}${amount}${items}${shipping}${firstName}${lastName}${email}${phone}${type}${continue_success_url}${currency}${return_params}`;
+  const dataToHash = `${req_time}${ABA_PAYWAY_MERCHANT_ID}${tran_id}${amount}${items}${shipping}${firstName}${lastName}${email}${phone}${type}${continue_success_url}${currency}${return_params}`;
   const hash = generateHash(dataToHash, ABA_PAYWAY_API_KEY);
-  res.send(
-    `
+
+  res.send(`
     <form method="POST" action="${ABA_PAYWAY_API_URL}" id="aba_merchant_request">
-    <input type="hidden" name="hash" value="${hash}" />
-    <input type="hidden" name="tran_id" value="${tran_id}" />
-    <input type="hidden" name="amount" value="${amount}" />
-    <input type="hidden" name="req_time" value="${req_time}" />
-    <input type="hidden" name="merchant_id" value="${ABA_PAYWAY_MERCHANT_ID}" />
-    <input type="hidden" name="firstname" value="${firstName}" />
-    <input type="hidden" name="lastname" value="${lastName}" />
-    <input type="hidden" name="phone" value="${phone}" />
-    <input type="hidden" name="email" value="${email}" />
-    <input type="hidden" name="items" value="${items}" />
-    <input type="hidden" name="return_params" value="${return_params}" />    
-    <input type="hidden" name="shipping" value="${shipping}" />
-    <input type="hidden" name="payment_option" value="" />
-    <input type="hidden" name="type" value="${type}" />    
-    <input type="hidden" name="continue_success_url" value="${continue_success_url}" />
-    <input type="hidden" name="currency" value="${currency}" />
-    <input type="hidden" name="return_param" value="${return_params}" />
+      <input type="hidden" name="hash" value="${hash}" />
+      <input type="hidden" name="tran_id" value="${tran_id}" />
+      <input type="hidden" name="amount" value="${amount}" />
+      <input type="hidden" name="req_time" value="${req_time}" />
+      <input type="hidden" name="merchant_id" value="${ABA_PAYWAY_MERCHANT_ID}" />
+      <input type="hidden" name="firstname" value="${firstName}" />
+      <input type="hidden" name="lastname" value="${lastName}" />
+      <input type="hidden" name="phone" value="${phone}" />
+      <input type="hidden" name="email" value="${email}" />
+      <input type="hidden" name="items" value="${items}" />
+      <input type="hidden" name="return_params" value="${return_params}" />    
+      <input type="hidden" name="shipping" value="${shipping}" />
+      <input type="hidden" name="payment_option" value="" />
+      <input type="hidden" name="type" value="${type}" />    
+      <input type="hidden" name="continue_success_url" value="${continue_success_url}" />
+      <input type="hidden" name="currency" value="${currency}" />
+      <input type="hidden" name="return_param" value="${return_params}" />
     </form>
     <script>
-    document.getElementById('aba_merchant_request').submit();
-    </script>`
-  );
+      document.getElementById('aba_merchant_request').submit();
+    </script>
+  `);
 });
 
-app.listen(PORT, () => console.log(`server is up on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is up on port: ${PORT}`));
